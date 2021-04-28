ef955da69093602d33d9a516fe679fa8e1cbedce
joaoprferreira <joaof082@gmail.com>
2021-04-28 18:07:57 -0300

    Starting the Player component

---
+++ src/Components/Player/index.tsx
@@ -0,0 +1,8 @@
+import styles from './styles.module.scss';
+
+export function Player() {
+  return (
+    <div className={styles.playerContainer}>
+    </div>
+  );
+ }
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,20 @@
 import "../styles/global.scss"
 
+import { Header } from "../Components/Header";
+import { Player } from "../Components/Player";
+
+import styles from '../styles/app.module.scss'
+
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <div className={styles.wrapper}>
+      <main>
+        <Header />
+        <Component {...pageProps} />
+      </main>
+      <Player />
+    </div>
+  )
 }
 
 export default MyApp
- 
